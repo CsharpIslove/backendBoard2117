fadb48936d2feac31ed39c1862dff4379cbb0cca
timurseitov <93837567+timurseitov@users.noreply.github.com>
2022-05-20 20:37:57 +0600

    asd

--- config/database.config.js
+++ config/database.config.js
@@ -1,3 +1,4 @@
 module.exports = {
-    url: 'mongodb://localhost:27017/crud-node-express'
+   // url: 'mongodb://localhost:27017/crud-node-express'
+    url: 'mongodb+srv://timur:1037895462@cluster0.5eqyb.mongodb.net/crud-node-express'
 }
