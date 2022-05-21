c5598def1a5a306351a134e92d9cc33325fe98a0
timurseitov <93837567+timurseitov@users.noreply.github.com>
2022-05-21 10:18:37 +0600

    s

--- config/database.config.js
+++ config/database.config.js
@@ -1,4 +1,5 @@
 module.exports = {
    // url: 'mongodb://localhost:27017/crud-node-express'
-    url: 'mongodb+srv://timur:1037895462@cluster0.5eqyb.mongodb.net/crud-node-express'
+    //url: 'mongodb+srv://timur:1037895462@cluster0.5eqyb.mongodb.net/crud-node-express'
+    url: 'mongodb+srv://admin:admin123@cluster0.qc2s4.mongodb.net/crud-node-express'
 }
